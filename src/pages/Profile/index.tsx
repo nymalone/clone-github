e4b76892c5f4436609c65e0d4b3edf02195ff758
Nykolle Malone <malone.nykolle@gmail.com>
2020-08-19 06:44:54 -0300

    fetch api profile and repos done -> need to change repo page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 import {
   Container,
@@ -15,12 +16,57 @@ import ProfileData from "../../components/ProfileData";
 import RepoCard from "../../components/RepoCard";
 import RandomCalendar from "../../components/RandomCalendar";
 
+import { APIUser, APIRepo } from "../../@types";
+interface Data {
+  user?: APIUser;
+  repos?: APIRepo[];
+  error?: string;
+}
+
 const Profile: React.FC = () => {
+  const { username = "nymalone" } = useParams(); // se a pessoa digita /nymalone ele vai pegar o nymalone e armazenar na variável username;
+  const [data, setData] = useState<Data>();
+
+  useEffect(() => {
+    Promise.all([
+      fetch(`https://api.github.com/users/${username}`),
+      fetch(`https://api.github.com/users/${username}/repos`),
+    ]).then(async (responses) => {
+      // console.log([await responses[0].json(), await responses[1].json()])
+      const [userResponse, reposResponse] = responses;
+
+      if (userResponse.status === 404) {
+        setData({ error: "User not found!" });
+        return;
+      }
+
+      const user = await userResponse.json();
+      const repos = await reposResponse.json();
+
+      const shuffledRepos = repos.sort(() => 0.50 - Math.random()); // 50% de chances de cada repositório aparecer
+      const slicedRepos = shuffledRepos.slice(0, 6); // quero que mostre em tela apenas 6 repositórios aleatórios
+
+      setData({
+        user,
+        repos: slicedRepos,
+      });
+    });
+  }, [username]);
+
+  if (data?.error) {
+    return <h1>{data.error}</h1>;
+  }
+
+  //se não carregou o usuario e não caregou os repositórios eu vou retonar o loading
+  if (!data?.user || !data?.repos) {
+    return <h1>Loading...</h1>
+  }
+
   const TabContent = () => (
     <div className="content">
       <RepoIcon />
       <span className="label">Repositories</span>
-      <span className="number">90</span>
+  <span className="number">{data.user?.public_repos}</span>
     </div>
   );
 
@@ -37,15 +83,15 @@ const Profile: React.FC = () => {
       <Main>
         <LeftSide>
           <ProfileData
-            username={"nymalone"}
-            name={"Nykolle Malone"}
-            avatarUrl={"https://avatars2.githubusercontent.com/u/54912285?v=4"}
-            followers={139}
-            following={31}
-            company={"Sami"}
-            location={"São Paulo, Brazil"}
-            email={"malone.nykolle@gmail.com"}
-            blog={"linkedin.com/in/nykollemalone"}
+            username={data.user.login}
+            name={data.user.name}
+            avatarUrl={data.user.avatar_url}
+            followers={data.user.followers}
+            following={data.user.following}
+            company={data.user.company}
+            location={data.user.location}
+            email={data.user.email}
+            blog={data.user.company}
           />
         </LeftSide>
 
@@ -58,17 +104,15 @@ const Profile: React.FC = () => {
             <h2>Random repos</h2>
 
             <div>
-              {[1, 2, 3, 4, 5, 6].map((n) => (
+              {data.repos.map((item) => (
                 <RepoCard
-                  key={n}
-                  username={"nymalone"}
-                  reponame={"aprova-ai-client"}
-                  description={
-                    "Projeto final desenvolvido no bootcamp Web Development Fullstack da IRONHACK - premiado como um dos melhores projetos."
-                  }
-                  language={n % 3 === 0 ? "Javascript" : "TypeScript"}
-                  stars={9}
-                  forks={3}
+                key={item.name}
+                username={item.owner.login}
+                reponame={item.name}
+                description={item.description}
+                language={item.language}
+                stars={item.stargazers_count}
+                forks={item.forks}
                 />
               ))}
             </div>
