84dee98f9d9b9ef9f419be8eb9ba9fb64132379a
Nykolle Malone <malone.nykolle@gmail.com>
2020-08-19 05:41:43 -0300

    repo page done

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
   return (
@@ -8,20 +18,40 @@ const Repo: React.FC = () => {
       <Breadcrumb>
         <RepoIcon />
 
-        <Link  className={'username'} to={'/nymalone'}>
+        <Link className={"username"} to={"/nymalone"}>
           nymalone
         </Link>
 
         <span>/</span>
 
-        <Link  className={'reponame'} to={'/nymalone/aprova-ai-client'}>
+        <Link className={"reponame"} to={"/nymalone/aprova-ai-client"}>
           aprova-ai-client
         </Link>
       </Breadcrumb>
 
-      <p>Projeto final desenvolvido no bootcamp Web Development Fullstack da IRONHACK - premiado como um dos melhores projetos.</p>
+      <p>
+        Projeto final desenvolvido no bootcamp Web Development Fullstack da
+        IRONHACK - premiado como um dos melhores projetos.
+      </p>
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>3</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/nymalone/aprova-ai-client"}>
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
     </Container>
-  )
-}
+  );
+};
 
 export default Repo;
